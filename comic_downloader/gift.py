ad57239da9625e38bf277d0878bd31260476653c
kingname <greensouth@foxmail.com>
2014-08-13 22:11:46 +0800

    卖肉漫画下载器v1.2
    解决界面卡顿问题，使用多线程下载技术
    修改更新日志

--- comic_downloader/gift.py
+++ comic_downloader/gift.py
@@ -2,8 +2,8 @@
 
 #本程序下载卖肉漫画
 #使用请翻墙
-#已经实现基本功能
-#已知bug: 下载漫画时，图形界面会卡死，但是仍然在继续下载。正在尝试解决。
+#大部分功能完成
+#做好以后，送个室友当生日礼物。
 
 import urllib    
 import urllib2
@@ -11,16 +11,15 @@ import re
 import sys
 import os
 import wx
-import chardet;
+import threading
+import random
 root = os.getcwd()
-verson = '1.00'
+verson = '1.20'
 author = 'kingname'
-down = 0
 reload(sys)
 sys.setdefaultencoding('utf-8')
-#website = 'http://cncaomm.com/read-htm-tid-4246106.html'
 page = 2
-url = 'http://cncaomm.com/thread-htm-fid-20-page-'
+url = 'http://50.117.115.70/thread-htm-fid-20-page-'
 headers = {
 	
 	       #"GET":url,
@@ -29,10 +28,34 @@ headers = {
 	       "User-Agent":"Mozilla/5.0 (Windows NT 6.2; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/35.0.1916.114 Safari/537.36" 
 }
 
+class WorkerThread(threading.Thread):
+	def __init__(self,img_url,comic_path):
+		threading.Thread.__init__(self)
+		self.url = img_url
+		self.comic_path = comic_path
+		self.leave = threading.Event()
+		self.leave.clear()		
+
+	def stop(self):
+		#self.leave.set()
+		self.thread_stop = True 
+		print "12345"
+
+	def run(self):
+		img_add = re.findall('<img src="(.*?)" border="0"',self.url,re.S)
+		num = 0
+		#for i in img_add:
+		while num< len(img_add):
+			i = img_add[num]
+			img_path = self.comic_path + '\\' + str(num) + '.' + i[-3:]
+			down_img(i,img_path)
+			wx.CallAfter(Setmessage,img_path)
+			num +=1	
+		frame.multiText2.AppendText(u'下载完成\n')	
 
 class myFrame(wx.Frame): 
 	def __init__(self):  
-			wx.Frame.__init__(self, None, -1, u'卖肉漫画下载器v1.00',   
+			wx.Frame.__init__(self, None, -1, u'卖肉漫画下载器v1.20',   
 				size=(780, 730))  
 			panel = wx.Panel(self, -1) 
 			basicLabel = wx.StaticText(panel, -1, u"漫画列表:",) 
@@ -45,7 +68,7 @@ class myFrame(wx.Frame):
 					u"",  
 					size=(350, 600),pos= (5,20), style=wx.TE_MULTILINE+wx.TE_READONLY) #创建一个文本控件
 			self.multiText2 = wx.TextCtrl(panel, -1,  
-					u"下载状态",  
+					u"下载状态\n",  
 					size=(380, 200),pos= (380,420), style=wx.TE_MULTILINE+wx.TE_READONLY) #创建一个文本控件
 			sizer = wx.FlexGridSizer(rows=3,cols=2, hgap=6, vgap=6)
 			sizer.AddMany([basicLabel])  
@@ -65,33 +88,46 @@ class myFrame(wx.Frame):
 			self.button_Onjust.SetDefault()
 			self.button_Ondown.SetDefault()
 			self.button_Oncancle.SetDefault()
-			
+	
+	#加载首页漫画列表		
 	def Onload(self, event):
 		#down_start(url)
 		addr = url + '2.html'
 		self.comic_list = get_list(addr)
-		#frame.multiText.AppendText(u"开始下载！\n")
+		
+
+	#加载下一页漫画列表
 	def Onnext(self,event):
 		global page
 		page += 1
 		frame.multiText.Clear() 
 		addr = url + str(page) + '.html'
 		self.comic_list = get_list(addr)
-	def Onjust(self,event):
+	
+	#上一页漫画列表
+	def Onjust(self,event):  
 		global page
 		if page >= 3:
 			page -=1
 			frame.multiText.Clear()
 			addr = url + str(page) + '.html'
 			self.comic_list = get_list(addr)
+	#下载漫画
 	def Ondown(self,event):
-		global down
-		down  = 1
 		comic_number = int(self.basicText.GetValue()) - 1
+		#Setmessage(u'开始下载!\n')
+		frame.multiText2.AppendText(u"开始下载！\n")
 		down_start(self.comic_list,comic_number)
+	
+	#取消下载
 	def  Oncancle(self,event):
-		global down
-		down = 0
+		thethread.stop()
+		#Setmessage(u'下载取消！')
+		frame.multiText2.AppendText(u'下载取消！\n')
+
+#发送信息到GUI节目
+def Setmessage(info):
+		frame.multiText2.AppendText(info.decode('gbk','ignore')+"\n")
 
 
 #获取页面源代码
@@ -100,33 +136,20 @@ def get_page_sourse(url):
 	for key in headers:
 		req.add_header(key,headers[key])
 	return urllib2.urlopen(req).read().decode('utf-8', 'ignore').encode('gb2312','ignore')
+
 #通过正则表达式获得含有图片的代码块
 def get_img_url(html):
 	img_group = re.findall('<div class="f14" id="read_tpc">(.*?)<tr class="r_one" id="att_info_display">',html,re.S)
 	return img_group
-#图像文件后缀名
-def name(address):
-	if address.find('.jpg'):
-		return 'jpg'
-	else:
-		return 'png'
 
 #下载图片漫画
-def down_img(img_group,comic_path):
-	img_add = re.findall('<img src="(.*?)" border="0"',img_group,re.S)
-	num = 0
-	for i in img_add:
-		if down == 1:
-			path2 = comic_path + '\\' + str(num) + '.' + 'jpg'
-			try:		
-				data = urllib.urlretrieve(i,path2)
-				frame.multiText2.AppendText(path2.decode('gbk','ignore')+"\n")
-				num += 1
-			except Exception as e:
-				frame.multiText2.AppendText(u'下载出错，跳过本张图片\n')
-		else:
-			frame.multiText2.AppendText(u'下载取消！\n')
-	frame.multiText2.AppendText(u'下载完成\n')
+def down_img(i,img_path):
+	try:		
+		data = urllib.urlretrieve(i,img_path)
+	except Exception as e:
+		frame.multiText2.AppendText(u'下载出错，跳过本张图片\n')
+		#print "err"
+		#Setmessage('下载出错，跳过本张图片！')
 
 #获取漫画列表
 def get_list(url):
@@ -134,8 +157,7 @@ def get_list(url):
 	list_group = re.findall('orderThreadsClass.orderThreads(.*?)<form action="thread.php',sourse,re.S)
 	page_address = re.findall('<a name=.*?></a><a href="(.*?)" id="a_ajax.*? class="subject">(.*?)</a>',list_group[0],re.S)
 	number = 1
-	#for i in page_address:
-	#	s = 'cncaomm.com/' + i[0]
+
 	for i in page_address:
 		if i[1][0] == '<':
 			name = i[1][23:-11]
@@ -147,57 +169,36 @@ def get_list(url):
 		frame.multiText.AppendText(name.decode('gbk','ignore')+"\n")
 	return page_address
 
-# 为每个漫画新建文件夹
+# 为每个漫画新建文件夹,如果名字重复，则文件夹名后面加上随机数字
 def to_make_dir(path):
 	new_path = os.path.join(root, path)
 	if os.path.isdir(new_path):
-		new_path += '_new'
+		new_path += str(random.randint(1,20))
 	os.makedirs(new_path)
 
-#开始下载
-def down_start_consloe(url):
-	n = 0
-	page_address = get_list(url)
-	for i in page_address:
-		if i[1][0] == '<':
-			name = i[1][23:-11]
-		else:
-			name = i[1]
-		frame.multiText.AppendText(name+"\n")
-		try:
-			to_make_dir(name)
-			
-			comic_path = root + '\\' + name
-		except Exception as e:
-			to_make_dir('comic'+str(n))	
 
-			comic_path = root + '\comic' + str(n)
-			n += 1
-		img_page_address = 'http://cncaomm.com/' + i[0]
-		print img_page_address
-		img_page_html = get_page_sourse(img_page_address)
-		img_url = get_img_url(img_page_html)
-		down_img(img_url[0],comic_path)
-
+#开始下载的函数
 def down_start(comic_list,comic_number):
-	n = 0
-	if comic_list[comic_number] =='<':
-		name = comic_list[comic][23:-11]
+	global thethread
+	if comic_list[comic_number][0] =='<':
+		name = comic_list[comic_number][23:-11]
 	else:
 		name = comic_list[comic_number][1]
 	try:
 		to_make_dir(name)
 		comic_path = root + '\\' + name
+
 	except Exception as e:
-		to_make_dir('comic'+str(n))	
-		comic_path = root + '\comic' + str(n)
-		n += 1
+		dir_name = 'comic'+str(random.randint(10,20))
+		to_make_dir(dir_name)	
+		comic_path = root + '\\' + dir_name
 	img_page_address = 'http://cncaomm.com/' + comic_list[comic_number][0]
 	img_page_html = get_page_sourse(img_page_address)
 	img_url = get_img_url(img_page_html)
-	down_img(img_url[0],comic_path)
+	thread = WorkerThread(img_url[0],comic_path)
+	thethread = thread
+	thread.start()
 
-#down_start(url)
 
 if __name__ == '__main__':
     app = wx.App()  
