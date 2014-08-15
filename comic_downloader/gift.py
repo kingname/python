19f60e3643655587098310c22f2d76018519306c
kingname <greensouth@foxmail.com>
2014-08-15 17:17:40 +0800

    卖肉漫画下载器v1.30
    更新在线预览漫画封面的功能

--- comic_downloader/gift.py
+++ comic_downloader/gift.py
@@ -14,11 +14,12 @@ import wx
 import threading
 import random
 root = os.getcwd()
-verson = '1.25'
+verson = '1.30'
 author = 'kingname'
 reload(sys)
 sys.setdefaultencoding('utf-8')
 page = 2
+down = 0
 url = 'http://50.117.115.70/thread-htm-fid-20-page-'
 headers = {
 	
@@ -29,10 +30,11 @@ headers = {
 }
 
 class WorkerThread(threading.Thread):
-	def __init__(self,img_url,comic_path):
+	def __init__(self,img_url,comic_path,preview = 0):
 		threading.Thread.__init__(self)
 		self.url = img_url
 		self.count = 0
+		self.preview = preview
 		self.comic_path = comic_path
 		self.leave = threading.Event()
 		self.leave.clear()		
@@ -45,9 +47,13 @@ class WorkerThread(threading.Thread):
 
 	def run(self):
 		img_add = re.findall('<img src="(.*?)" border="0"',self.url,re.S)
+
 		num = 0
 		#for i in img_add:
-		self.count = len(img_add)
+		if self.preview == 1:
+			self.count = 1
+		else:
+			self.count = len(img_add)
 		while num< self.count:
 			i = img_add[num]
 			img_path = self.comic_path + '\\' + str(num) + '.' + i[-3:]
@@ -58,37 +64,40 @@ class WorkerThread(threading.Thread):
 
 class myFrame(wx.Frame): 
 	def __init__(self):  
-			wx.Frame.__init__(self, None, -1, u'卖肉漫画下载器v1.25',   
+			wx.Frame.__init__(self, None, -1, u'卖肉漫画下载器v1.30',   
 				size=(780, 730))  
 			panel = wx.Panel(self, -1) 
 			basicLabel = wx.StaticText(panel, -1, u"漫画列表:",) 
-			basicLabel2 = wx.StaticText(panel, -1, u"下载状态：",(380,400)) 
-			basicLabel3 = wx.StaticText(panel, -1, u"在线看漫画功能下个版本开发",(480,200))
+			basicLabel2 = wx.StaticText(panel, -1, u"下载状态：",(380,420)) 
+			basicLabel3 = wx.StaticText(panel, -1, u"漫画首页预览",(480,200))
 			self.multiText2 = wx.TextCtrl(panel, -1,  
 					u"下载状态\n",  
-					size=(380, 200),pos= (380,420), style=wx.TE_MULTILINE+wx.TE_READONLY) #创建一个文本控件
+					size=(380, 180),pos= (380,440), style=wx.TE_MULTILINE+wx.TE_READONLY) #创建一个文本控件
 			self.comic_name_list = [] 
 			self.listBox = wx.ListBox(panel, -1, (5, 20), (350, 600), self.comic_name_list,   
 							wx.LB_SINGLE)
 			sizer = wx.FlexGridSizer(rows=3,cols=2, hgap=6, vgap=6)
 			sizer.AddMany([basicLabel])  
-			panel.SetSizer(sizer)
+			panel.SetSizerAndFit(sizer)
 			self.button_Onload = wx.Button(panel, -1, u"加载首页", pos=(130, 630)) 
 			self.button_Onnext = wx.Button(panel, -1, u"下一页>>", pos=(230, 630)) 
 			self.button_Onjust = wx.Button(panel, -1, u"<<上一页", pos=(30, 630)) 
-			self.button_Ondown = wx.Button(panel, -1, u"开始下载", pos=(380, 630)) 
-			self.button_Oncancle = wx.Button(panel, -1, u"停止下载", pos=(470, 630))
+			self.button_Ondown = wx.Button(panel, -1, u"开始下载", pos=(470, 630)) 
+			self.button_Oncancle = wx.Button(panel, -1, u"停止下载", pos=(560, 630))
+			self.button_Onpreview = wx.Button(panel, -1, u"预览首页", pos=(380, 630))
 			self.Bind(wx.EVT_BUTTON, self.Onload, self.button_Onload)
 			self.Bind(wx.EVT_BUTTON, self.Onnext, self.button_Onnext)
 			self.Bind(wx.EVT_BUTTON, self.Onjust, self.button_Onjust)
 			self.Bind(wx.EVT_BUTTON, self.Ondown, self.button_Ondown)
 			self.Bind(wx.EVT_BUTTON, self.Oncancle, self.button_Oncancle)
+			self.Bind(wx.EVT_BUTTON, self.Onpreview, self.button_Onpreview)
 			self.Bind(wx.EVT_CLOSE,  self.OnCloseWindow)
 			self.button_Onload.SetDefault()
 			self.button_Onnext.SetDefault()
 			self.button_Onjust.SetDefault()
 			self.button_Ondown.SetDefault()
 			self.button_Oncancle.SetDefault()
+			self.button_Onpreview.SetDefault()
 	
 	#加载首页漫画列表		
 	def Onload(self, event):
@@ -112,26 +121,44 @@ class myFrame(wx.Frame):
 			#frame.multiText.Clear()
 			addr = url + str(page) + '.html'
 			self.comic_list = get_list(addr)
+
+	#预览漫画首页
+	def Onpreview(self,event):
+		comic_number = self.listBox.GetSelections()
+		down_start(self.comic_list,list(comic_number)[0],preview = 1)
+		img2 = wx.Image('.\\preview\\0.jpg',wx.BITMAP_TYPE_ANY)
+		img1 = img2.Scale(370,400)
+		sb1 = wx.StaticBitmap(self, -1, wx.BitmapFromImage(img1),pos = (380,20))
+
 	#下载漫画
 	def Ondown(self,event):
+		global down
 		comic_number = self.listBox.GetSelections()
 		#Setmessage(u'开始下载!\n')
 		frame.multiText2.AppendText(u"开始下载！\n")
+		down = 1
 		down_start(self.comic_list,list(comic_number)[0])
 	
 	#取消下载
 	def  Oncancle(self,event):
-		thethread.stop()
-		#Setmessage(u'下载取消！')
-		frame.multiText2.AppendText(u'下载取消！\n')
+		global down
+		if down == 1:
+			thethread.stop()
+			#Setmessage(u'下载取消！')
+			frame.multiText2.AppendText(u'下载取消！\n')
+		else:
+			frame.multiText2.AppendText(u'还未下载，谈何取消？\n')
 
 	def OnCloseWindow(self, event):
-		thethread.stop()
+		global down
+		if down == 1:
+			thethread.stop()
+			down = 0
 		self.Destroy()
 
 #发送信息到GUI节目
 def Setmessage(info):
-		frame.multiText2.AppendText(info.decode('gbk','ignore')+"\n")
+	frame.multiText2.AppendText(info.decode('gbk','ignore')+"\n")
 
 
 #获取页面源代码
@@ -175,32 +202,42 @@ def get_list(url):
 	return page_address
 
 # 为每个漫画新建文件夹,如果名字重复，则文件夹名后面加上随机数字
-def to_make_dir(path):
+def to_make_dir(path,preview = 0):
 	new_path = os.path.join(root, path)
-	if os.path.isdir(new_path):
-		new_path += str(random.randint(1,20))
-	os.makedirs(new_path)
+	if not os.path.isdir(new_path):
+		os.makedirs(new_path)
+	else:
+	if preview == 0:
+		if os.path.isdir(new_path):
+			new_path += str(random.randint(1,20))
+		os.makedirs(new_path)
+		
 
 
 #开始下载的函数
-def down_start(comic_list,comic_number):
+def down_start(comic_list,comic_number,preview = 0):
 	global thethread
-	if comic_list[comic_number][0] =='<':
-		name = comic_list[comic_number][23:-11]
-	else:
-		name = comic_list[comic_number][1]
-	try:
-		to_make_dir(name)
+	if preview == 1:
+		name = 'preview'
+		to_make_dir(name,preview = 1)
 		comic_path = root + '\\' + name
+	else:
+		if comic_list[comic_number][0] =='<':
+			name = comic_list[comic_number][23:-11]
+		else:
+			name = comic_list[comic_number][1]
+		try:
+			to_make_dir(name)
+			comic_path = root + '\\' + name
 
-	except Exception as e:
-		dir_name = 'comic'+str(random.randint(10,20))
-		to_make_dir(dir_name)	
-		comic_path = root + '\\' + dir_name
+		except Exception as e:
+			dir_name = 'comic'+str(random.randint(10,20))
+			to_make_dir(dir_name)	
+			comic_path = root + '\\' + dir_name
 	img_page_address = 'http://cncaomm.com/' + comic_list[comic_number][0]
 	img_page_html = get_page_sourse(img_page_address)
 	img_url = get_img_url(img_page_html)
-	thread = WorkerThread(img_url[0],comic_path)
+	thread = WorkerThread(img_url[0],comic_path,preview)
 	thethread = thread
 	thread.start()
 
